1e88b2b4c8b65a3056677e2ae43b8624c8d31754
Deoth <gueideoth@gmail.com>
2025-03-19 14:42:24 -0400

    add user registration and status views, implement user role handling, and enhance error responses

--- backend/normalauthentication/views.py
+++ backend/normalauthentication/views.py
@@ -19,3 +19,62 @@ class LoginView(APIView):
             return Response({'error': False, } , status=status.HTTP_200_OK)
             
         return Response({'error': 'Identifiants invalides'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+#   CREATION DE COMPTE UTILISATEUR LOGEIQUE
+class RegisterNow(APIView):
+    def post(self, request):
+        #print(request.data)
+        data = {
+            'username': request.data['contact'],
+            'email': str(request.data['email']).lower(),
+            'password': request.data['password_v'],
+            'first_name': request.data['first_name'],
+            'last_name': str(request.data['last_name']).upper()
+        }
+        status = request.data['statu']
+        noms = f"{str(request.data['last_name']).upper()} {str(request.data['first_name'])}"
+        serializers = UserSerializers(data=data)
+        
+        if serializers.is_valid():
+            serializers.save()
+            if int(status) == 1:
+                landlord_user = User.objects.get(username=request.data['contact'])
+                Landlord.objects.create(
+                    user=landlord_user,
+                    noms=noms,
+                    contact=request.data['contact']
+                )
+                return Response({'error': False, 'isRegistered': True, 'status': int(status)})
+            elif int(status) == 2:
+                client_user = User.objects.get(username=request.data['contact'])
+                Client.objects.create(
+                    user=client_user,
+                    noms=noms,
+                    contact=request.data['contact']
+                )
+                return Response({'error': False, 'isRegistered': True, 'status': int(status)})
+            return Response({'error': True, 'isRegistered': False})
+        print(serializers.errors)
+        return Response({'error': True, 'isRegistered': False})
+
+
+class UserStatus(APIView):
+    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly]
+    authentication_classes = [TokenAuthentication, ]
+
+    def post(self, request):
+        print(f"Token: {request.data['token']}")
+        try:
+            user = User.objects.get(auth_token=request.data['token'])
+            print(f"User: {user}")
+            try:
+                user.landlord
+                status = 1
+            except:
+                user.client
+                status = 2
+            response_msg = {'error': False, 'status': int(status)}
+        except AssertionError as e:
+            response_msg = {'error': True, 'type: ': str(e)}
+        return Response(response_msg)
