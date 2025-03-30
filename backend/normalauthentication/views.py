37111ce085fe1d47380edc0ee12ef68f3f9a821a
Deoth <gueideoth@gmail.com>
2025-03-30 05:27:55 -0400

    add authentication permissions and token authentication to normal authentication views

--- backend/normalauthentication/views.py
+++ backend/normalauthentication/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.authentication import TokenAuthentication
 from django.contrib.auth import authenticate, login
 from .serializers import UserSerializers
 
